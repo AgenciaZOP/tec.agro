d51f68d2cbbf391a10b603fe087722da53e88fbb
nandobfer <nandobfer@yahoo.com.br>
2023-07-17 08:06:43 -0300

    feat: added initial components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import "./App.css"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Login } from "./pages/Login"
+import { useMuiTheme } from "./hooks/useMuiTheme"
+import { ThemeProvider } from "@mui/material"
+import { Providers } from "./components/Providers"
 
 function App() {
+    const theme = useMuiTheme()
+
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<Login />} />
-            </Routes>
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <Providers>
+                <BrowserRouter>
+                    <Routes>
+                        <Route index element={<Login />} />
+                    </Routes>
+                </BrowserRouter>
+            </Providers>
+        </ThemeProvider>
     )
 }
 
