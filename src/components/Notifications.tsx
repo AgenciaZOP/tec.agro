2918c279e0f736ce2103377e400dbf822ffbb25c
tainarasn <tainarasn.sba@gmail.com>
2023-07-17 13:02:58 -0300

    Revert "feat: styling page notification"
    This reverts commit a92b8962c64c2f53e4570138ced305d6fb2bfa0b.

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -1,54 +1,15 @@
-import React, { useState } from "react"
-import { Box, Drawer, MenuItem, SxProps } from "@mui/material"
+import React from "react"
+import { Box, Drawer, MenuItem } from "@mui/material"
 import { useNotifications } from "../hooks/useNotifications"
-import profile from "../assets/notification.jpeg"
 
 interface NotificationsProps {}
 
 export const Notifications: React.FC<NotificationsProps> = ({}) => {
     const { open, setOpen } = useNotifications()
 
-    const [quantity, setQuantity] = useState(30)
-    const [name, setName] = useState("Omar Donin")
-    const [time, settime] = useState(4)
-
     const handleClose = () => {
         setOpen(false)
     }
 
-    return (
-        <Drawer anchor={"right"} open={open} onClose={handleClose} PaperProps={{ sx: { width: "80vw" } }}>
-            <Box sx={{ padding: "8vw", display: "flex", flexDirection: "column", gap: "5vw" }}>
-                <Box sx={{ display: "flex", flexDirection: "column" }}>
-                    <h3>Notificações</h3>
-                    <p style={{ fontSize: "3.5vw" }}>
-                        Você tem <span style={{ fontWeight: "bold" }}>{quantity} notificações </span> hoje
-                    </p>
-                </Box>
-                <Box sx={{ display: "flex", flexDirection: "column", gap: "5vw" }}>
-                    <p style={{ fontSize: "3.7vw", fontWeight: "bolder" }}>Hoje</p>
-                    <Box sx={{ display: "flex", flexDirection: "row", gap: "4vw" }}>
-                        <Box
-                            sx={{
-                                borderRadius: "50%",
-                                width: "12vw",
-                                height: "10vw",
-                            }}
-                        >
-                            <img src={profile} alt="" style={{ width: "80vw " }} />
-                        </Box>
-                        <Box sx={{ display: "flex", flexDirection: "column" }}>
-                            <p style={{ fontSize: "3vw" }}>
-                                <span style={{ fontWeight: "bold" }}>{name}</span> lhe enviou uma mensagem
-                            </p>
-                            <p style={{ color: "gray", fontSize: "2.5vw" }}>{time} h atrás</p>
-                        </Box>
-                    </Box>
-                </Box>
-                <Box sx={{ display: "flex", flexDirection: "column" }}>
-                    <p style={{ fontSize: "3.7vw", fontWeight: "bolder" }}>Esta Semana</p>
-                </Box>
-            </Box>
-        </Drawer>
-    )
+    return <Drawer anchor={"right"} open={open} onClose={handleClose} PaperProps={{ sx: { width: "80vw" } }}></Drawer>
 }
