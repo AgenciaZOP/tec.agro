ccd773bb368484e40126c2a933e6e17dc01462ee
ispadilha <israel.padilha@gmail.com>
2023-07-20 03:44:54 -0300

    feat: chat search

--- src/pages/Chats.tsx
+++ src/pages/Chats.tsx
@@ -3,6 +3,8 @@ import { Box } from "@mui/material"
 import { useChats } from "../hooks/useChats"
 import { ChatCard } from "../components/ChatCard"
 import { Chat } from "../components/Chat"
+import { SearchInput } from "../components/SearchInput"
+import { useUser } from "../hooks/useUser"
 
 interface ChatsProps {
     channel: string
@@ -10,21 +12,26 @@ interface ChatsProps {
 
 export const Chats: React.FC<ChatsProps> = ({ channel }) => {
     const chats = useChats().getChannel(channel)
-
+    const {user} = useUser()
+    
+    const [list, setList] = useState(chats)
     const [currentChat, setCurrentChat] = useState<Chat>()
+
+    const handleChange = (value: string) => {
+        const result = chats.filter((chat) => chat.users.find(item => item.id != user?.id)!.name.toLowerCase().includes(value.toLowerCase()))
+        setList(result)
+    }
 
     useEffect(() => {
       console.log(chats)
-    
-      
     }, [chats])
-    
 
     return currentChat ? (
         <Chat chat={currentChat} />
     ) : (
         <Box sx={{ flexDirection: "column", gap: "4vw" }}>
-            {chats.map((chat) => (
+            <SearchInput placeholder="conversas" onChange={handleChange} />
+            {list.map((chat) => (
                 <ChatCard key={chat.id} chat={chat} onClick={() => setCurrentChat(chat)} />
             ))}
         </Box>
