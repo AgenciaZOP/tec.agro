cc3967602f896a8a838e057e5eada7973a53de78
nandobfer <nandobfer@yahoo.com.br>
2023-07-24 17:36:30 -0300

    update: build script

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ user="agenciaboz"
 subdomain="tec.agro.agenciaboz.com.br"
 path="/home/${user}/${subdomain}"
 
-yarn build
+npx vite build
 echo 'Uploading build to server'
 scp -r -P 22022 dist/* agenciaboz:${path}
 ssh -p 22022 agenciaboz "chown -R ${user}:${user} ${path}/*"
