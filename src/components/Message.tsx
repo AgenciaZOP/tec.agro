c58d386fecb717b186512326893c1fa88885b6ff
ispadilha <israel.padilha@gmail.com>
2023-07-25 11:06:03 -0300

    Started structuring Message component

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,10 +1,19 @@
 import React from "react"
-import { Box } from "@mui/material"
+import { Avatar, Box } from "@mui/material"
 
 interface MessageProps {
     message: Message
 }
 
 export const Message: React.FC<MessageProps> = ({ message }) => {
-    return <Box sx={{}}>{message.text}</Box>
+    return (
+        <Box sx={{ margin: "4vw", gap: "2vw" }}>
+            <Avatar sx={{ alignSelf: "flex-end" }} />
+            <div className="message-texts-column" style={{ flexDirection: "column", width: "70vw", gap: "2vw" }}>
+                {/* {message.text} */}
+                <p className="message-name" style={{ color: "grey" }}>Fulano de Tal</p>
+                <p className="message-text" style={{ backgroundColor: "white", padding: "4vw", borderRadius: "4vw" }} >Integer eget aliquet nibh praesent tristique magna sit amet purus. Est sit amet facilisis magna etiam tempor orci.</p>
+            </div>
+        </Box>
+    )
 }
