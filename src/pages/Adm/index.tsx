6c49cff4fe15f1791f09eaf63f281209ed8c3659
Vitoria F <freitasvitoria712@gmail.com>
2023-08-01 10:13:07 -0300

    [add] reviewProfile in adm

--- src/pages/Adm/index.tsx
+++ src/pages/Adm/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Box } from "@mui/material"
+import { Box, makeStyles } from "@mui/material"
 import { Header } from "../../components/Header"
 import { BottomNavigation } from "../../components/BottomNavigation"
 import { Route, Routes } from "react-router-dom"
@@ -12,20 +12,27 @@ interface AdmProps {
     user: User
 }
 
+
+
 export const Adm: React.FC<AdmProps> = ({ user }) => {
     const bottomMenu = useNavigationList()
 
     return (
-        <Box sx={{ flexDirection: "column", width: "100%" }}>
+        <Box sx={{
+            flexDirection: "column", width: "100%", height: "100%", overflowY: "auto",
+        }}>
             <Header />
-            <Box sx={{ width: "100%" }}>
+            <Box sx={{
+                height: "100%", width: "100%"
+            }}>
                 <Routes>
                     <Route index element={<Panel />} />
                     <Route path="reviews" element={<Reviews />} />
                     <Route path="review" element={<ReviewProfile />} />
                 </Routes>
             </Box>
-            <BottomNavigation section={bottomMenu.admin} />
+            <BottomNavigation
+                section={bottomMenu.admin} />
         </Box>
     )
 }
