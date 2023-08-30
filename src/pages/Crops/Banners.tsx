cf281ed649fb10d02d0bcdf8f6f0ac07ee8032ff
ispadilha <israel.padilha@gmail.com>
2023-08-30 10:19:22 -0300

    fix: scrolling boxes in home/crops

--- src/pages/Crops/Banners.tsx
+++ src/pages/Crops/Banners.tsx
@@ -7,7 +7,7 @@ interface BannersProps {
 
 export const Banners: React.FC<BannersProps> = ({ images }) => {
     return (
-        <Box sx={{ width: "100vw", overflowX: "auto", gap: "3vw", paddingRight: "10vw", paddingBottom: "2vw" }}>
+        <Box sx={{ width: "100vw", overflowX: "auto", gap: "3vw", marginLeft: "-4vw", padding: "0 4vw 1.1vw" }}>
             {images.map((image) => (
                 <Avatar key={image} variant="rounded" src={image} sx={{ width: "80vw", height: "40vw", borderRadius: "5vw", boxShadow: "0 0.5vw 1vw gray" }} />
             ))}
--- src/pages/Crops/NearYouList.tsx
+++ src/pages/Crops/NearYouList.tsx
@@ -13,7 +13,7 @@ export const NearYouList: React.FC<NearYouListProps> = ({}) => {
     return (
         <Box sx={{ flexDirection: "column", gap: "2vw" }}>
             <ListTitle title="Safras" location="crops" />
-            <Box sx={{ width: "100vw", overflowX: "auto", paddingRight: "10vw", gap: "4vw" }}>
+            <Box sx={{ width: "100vw", overflowX: "auto", paddingRight: "10vw", gap: "4vw", marginLeft: "-4vw", padding: "0 4vw" }}>
                 {crops.map((crop) => (
                     <CropCard key={crop.id} crop={crop} variant="square" />
                 ))}
