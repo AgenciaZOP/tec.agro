e1f15511c1e541e8ffd272d6485e2b0a53359f17
nandobfer <nandobfer@yahoo.com.br>
2023-09-15 12:01:13 -0300

    update: removed tabs from reviews

--- src/pages/Adm/Reviews.tsx
+++ src/pages/Adm/Reviews.tsx
@@ -14,25 +14,18 @@ export const Reviews: React.FC<ReviewsProps> = ({}) => {
     const header = useHeader()
     const navigate = useNavigate()
     const { businesses } = useBusinesses()
-    const [value, setValue] = useState(1)
 
     const category: Category = {
         id: 0,
         name: "Máquinas",
     }
     // adicionar as outras subaccounts e ordenar por data?
-    const pending = [
-        ...businesses.filter((business) => !business.active).map((item) => ({ ...item, type: "Loja & Serviços" })),
-    ]
+    const pending = [...businesses.filter((business) => !business.active).map((item) => ({ ...item, type: "Loja & Serviços" }))]
 
     const [list, setList] = useState(pending)
 
     const handleSearch = (value: string) => {
         setList(pending.filter((item) => item.name.toLowerCase().includes(value.toLowerCase())))
-    }
-
-    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-        setValue(newValue)
     }
 
     useEffect(() => {
@@ -44,41 +37,17 @@ export const Reviews: React.FC<ReviewsProps> = ({}) => {
             <SearchInput placeholder="análises" onChange={handleSearch} />
 
             {/* <ListTitle title="Categorias em análise" location="categories" /> */}
-            
-                <Box sx={{ width: "100%" }}>
-                    <Tabs
-                        value={value}
-                        onChange={handleChange}
-                        textColor="primary"
-                        indicatorColor="primary"
-                        aria-label="secondary tabs example"
-                        sx={{ width: "100%" }}
-                    >
-                        <Tab sx={{ width: "50%", fontSize: "3.2vw" }} value={1} label="Negócios e Serviços" />
-                        <Tab sx={{ width: "50%", fontSize: "3.2vw" }} value={2} label="Categorias" />
-                    </Tabs>
-            </Box>
-            <Box sx={{width:"100%", flexDirection: "column",height:"100%", paddingBottom:"6vh", overflow:"auto"}}>
-                {value === 1 && (
-                    <Box sx={{ flexDirection: "column", gap: "3vw", width: "100%" }}>
-                        {list.map((business) => (
-                            <ReviewCard
-                                key={business.id}
-                                subaccount={business}
-                                onClick={() => navigate("/adm/review", { state: { profile: business } })}
-                            />
-                        ))}
-                    </Box>
-                )}
-                {value === 2 && (
-                    <Box sx={{ flexDirection: "column", gap: "2vw" }}>
-                        {list.map((business) => (
-                            <CardCategory key={business.id} activeCategory={false} category={category}/>
-                        ))}
-                        
-                        
-                    </Box>
-                )}
+
+            <Box sx={{ width: "100%", flexDirection: "column", height: "100%", paddingBottom: "6vh", overflow: "auto" }}>
+                <Box sx={{ flexDirection: "column", gap: "3vw", width: "100%" }}>
+                    {list.map((business) => (
+                        <ReviewCard
+                            key={business.id}
+                            subaccount={business}
+                            onClick={() => navigate("/adm/review", { state: { profile: business } })}
+                        />
+                    ))}
+                </Box>
             </Box>
         </Box>
     )
