f3d2abde457f7e0f26d361cf0172465deffccb13
ispadilha <israel.padilha@gmail.com>
2023-09-18 12:58:11 -0300

    feat: clear filters button

--- src/components/FiltersDrawer.tsx
+++ src/components/FiltersDrawer.tsx
@@ -1,11 +1,12 @@
 import React from "react"
-import { Box, Drawer, IconButton } from "@mui/material"
+import { Box, Button, Drawer, IconButton } from "@mui/material"
 import { useFiltersDrawer } from "../hooks/useFiltersDrawer"
 import CloseSharpIcon from "@mui/icons-material/CloseSharp"
 import { backdropStyle } from "../style/backdrop"
 import { StatesList } from "./StatesList"
 import { DistanceSlider } from "./DistanceSlider"
 import { CategoriesList } from "../pages/Crops/CategoriesList"
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 
 interface FiltersDrawerProps {}
 
@@ -24,7 +25,7 @@ export const FiltersDrawer: React.FC<FiltersDrawerProps> = ({}) => {
             PaperProps={{ sx: { width: "80vw" } }}
             ModalProps={{ BackdropProps: { sx: backdropStyle } }}
         >
-            <Box sx={{ padding: "4vw", display: "flex", flexDirection: "column", height: "60%", justifyContent: "space-between", gap: "5vw" }}>
+            <Box sx={{ padding: "4vw", display: "flex", flexDirection: "column", height: "100%", justifyContent: "space-between", gap: "5vw" }}>
                 <Box sx={{ alignItems: "center", justifyContent: "space-between" }}>
                     <Box sx={{ display: "flex", flexDirection: "column", gap: "1vw" }}>
                         <h3 style={{ fontWeight: "500" }}>Filtros</h3>
@@ -62,6 +63,27 @@ export const FiltersDrawer: React.FC<FiltersDrawerProps> = ({}) => {
                     <h4>Categorias</h4>
                     <CategoriesList />
                     <hr />
+                </Box>
+                <Box
+                    sx={{
+                        justifyContent: "center"
+                    }}    
+                >
+                    <Button variant="outlined"
+                        sx={{
+                            borderRadius: "12vw",
+                            alignItems: "center",
+                            textTransform: "none",
+                        }}
+                    >
+                            <DeleteOutlineIcon sx={{ height: "6vw", width: "6vw" }} />
+                            <Box
+                                sx={{
+                                    marginBottom: "-0.5vw"
+                                }}
+                            >
+                                Limpar Filtros</Box>
+                    </Button>
                 </Box>
             </Box>
         </Drawer>
