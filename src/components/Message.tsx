76ed942cac3d1572c16502ea56dd8038d2c9ec70
ispadilha <israel.padilha@gmail.com>
2023-09-18 08:24:23 -0300

    Changed Message component to render message.text

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -10,9 +10,8 @@ export const Message: React.FC<MessageProps> = ({ message }) => {
         <Box sx={{ margin: "6vw", gap: "2vw", fontSize: "4vw", justifyContent: "space-between" }}>
             <Avatar sx={{ alignSelf: "flex-end" }} />
             <div className="message-texts-column" style={{ flexDirection: "column", width: "60vw", gap: "2vw" }}>
-                {/* {message.text} */}
                 <p className="message-name" style={{ color: "#868686", margin: "0 0 0 4vw" }}>Fulano de Tal</p>
-                <p className="message-text" style={{ backgroundColor: "#EEFFFE", padding: "4vw", borderRadius: "4vw 4vw 4vw 0" }} >Integer eget aliquet nibh praesent tristique magna sit amet purus. Est sit amet facilisis magna etiam tempor orci.</p>
+                <p className="message-text" style={{ backgroundColor: "#EEFFFE", padding: "4vw", borderRadius: "4vw 4vw 4vw 0" }} >{ `${message.text}` }</p>
             </div>
             <p className="message-time" style={{ color: "#868686", alignSelf: "flex-end" }}>12:37</p>
         </Box>
